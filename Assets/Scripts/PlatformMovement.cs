d12e563ebb0725f18e5dd1d175de8f7d15c7bbed
Hetvi <hetvisha@usc.edu>
2023-10-06 02:17:54 -0700

    added first platform

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
+   [SerializeField] private GameObject[] points;
+   private int index = 0;
+
+   [SerializeField] private float speed = 4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class PlatformMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (
+            Vector2.Distance(
+                points[index].transform.position,
+                transform.position
+            ) < 0.1f
+        )
+        {
+            index ++;
+            if (index >= points.Length) {
+                index = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[index].transform.position, Time.deltaTime * speed);
     }
 }
