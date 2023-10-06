3a16a0430427bd4e82403142c1da1e6bbf6755b6
Money <money.chaudhary@thehotspring.com>
2023-10-05 22:46:23 -0700

    added jump movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space")) {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+        }
     }
 }
