d222ad43971395442356c4181a3fb8f4c41fdb0a
Utsav <ugarg@usc.edu>
2023-10-06 01:39:06 -0700

    Added Spike Traps

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Trap")) {
+             rigidBody.bodyType = RigidbodyType2D.Static;
+             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
