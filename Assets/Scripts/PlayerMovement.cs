b0d9b99b3f97ac369bde8afc5d65507fe8d7f95b
Utsav <ugarg@usc.edu>
2023-10-05 23:47:17 -0700

    Allow jump only when grounded

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
     [SerializeField] private float speed = 6f;
     [SerializeField] private float jump = 8f;
+    [SerializeField] private LayerMask jumpableGround;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -19,8 +23,19 @@ public class PlayerMovement : MonoBehaviour
        float x = Input.GetAxisRaw("Horizontal");
        rigidBody.velocity = new Vector2(x * speed, rigidBody.velocity.y);
 
-        if (Input.GetButtonDown("Jump")) {
+        if (Input.GetButtonDown("Jump") && IsGrounded()) {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
         }
+    }
+
+    private bool IsGrounded() {
+        return Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0f,
+            Vector2.down,
+            0.1f,
+            jumpableGround
+        );
     }
 }
