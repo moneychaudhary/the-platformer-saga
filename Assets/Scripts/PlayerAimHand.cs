32eb1c557b7dc96c975e1300419397382ca0b297
Utsav Garg <utsav17garg@gmail.com>
2023-10-19 15:50:14 -0700

    Adding_Bullets_With_Colors

---
+++ Assets/Scripts/PlayerAimHand.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using System;
+
+public class PlayerAimHand : MonoBehaviour
+{
+    private Transform aimTransform;
+    private Transform aimGunEndPoint;
+    public event EventHandler<OnShootEventArgs> OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        public Vector3 shootPosition;
+    }
+    public int speed = 3;
+    public GameObject BulletPrefab;
+    public GameObject startLocation;
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        aimGunEndPoint = aimTransform.Find("GunEndPoint");
+        Debug.Log(aimGunEndPoint);
+}
+
+    private void Update()
+    {
+        HandleAim();
+        HandleShooting();
+    }
+
+    private void HandleAim()
+    {
+        Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+            OnShoot?.Invoke(this, new OnShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPoint.position,
+                shootPosition = mousePosition,
+            });
+            shoot();
+        }
+    }
+
+    public void shoot()
+    {
+        Debug.Log(aimGunEndPoint.position);
+        Debug.Log(startLocation.transform.position);
+        Debug.Log(startLocation.transform.rotation);
+        GameObject bullet = Instantiate(BulletPrefab, startLocation.transform.position, Quaternion.Euler(new Vector3(startLocation.transform.rotation.x, startLocation.transform.rotation.y, startLocation.transform.rotation.z  - 90)));
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        SpriteRenderer bulletSprite = bullet.GetComponent<SpriteRenderer>();
+        bulletSprite.color = Color.red;
+        rb.velocity = startLocation.transform.right * speed;
+    }
+
+}
