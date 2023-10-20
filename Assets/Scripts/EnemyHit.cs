1ec95f52ed0c1fcbf39f885126df3f1481a0ebde
Aditya-1500 <adityamanchanda15@gmail.com>
2023-10-19 21:11:41 -0700

    Add Health Bars

--- Assets/Scripts/EnemyHit.cs
+++ Assets/Scripts/EnemyHit.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHit : MonoBehaviour
 {
+    private RectTransform healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthBar = GetComponent<RectTransform>();
+        UpdateHealthBar(Health.enemyHealth);
     }
 
     // Update is called once per frame
@@ -20,6 +23,21 @@ public class EnemyHit : MonoBehaviour
         if (collision.gameObject.tag == "Bullet") {
             Debug.Log("Enemy hit!");
             Destroy(collision.gameObject);
+            Damange(0.1f);
         }
+    }
+
+    public void Damange(float damage) {
+        if(Health.enemyHealth - damage >= 0f) {
+            Health.enemyHealth -= damage;
+        } else {
+            Health.enemyHealth = 0f;
+        }
+
+        UpdateHealthBar(Health.enemyHealth);
+    }
+
+    public void UpdateHealthBar(float size) {
+        healthBar.localScale = new Vector3(size, 1f);
     }
 }
